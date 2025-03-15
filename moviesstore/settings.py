83920fcd6fcbf7cba05e04e8cc2793e50cd68909
gb <gbaldassin@yahoo.co.uk>
2025-03-15 14:37:23 +0100

    full project

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
+    'accounts',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviesstore/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
